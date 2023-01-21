9c83f51ccc5e82fa525d65d9dc58ffd2553b638e
Doctor I. Q. Lightbulb <64180446+doctorLightbulb@users.noreply.github.com>
2023-01-21 14:58:42 -0800

    Displayed project cards dynamically

--- javaScript-files/code.js
+++ javaScript-files/code.js
@@ -53,3 +53,57 @@ function closePrivacyPopUp() {
     y.style.top = "50%";
     y.style.opacity = "0%";
 }
+
+const projectsList = [{
+    title: "Project A",
+    overlayText: "View the contents of Project A.",
+    cardImage: "https://www.kasandbox.org/programming-images/landscapes/lake.png",
+    imageAltText: "A beautiful landscape from Khan Academy's sandbox.",
+    linkURL: "./projects/project-a.html",
+},
+{
+    title: "Project B",
+    overlayText: "View the contents of Project B.",
+    cardImage: "https://www.kasandbox.org/programming-images/landscapes/lake.png",
+    imageAltText: "A beautiful landscape from Khan Academy's sandbox.",
+    linkURL: "./projects/project-b.html",
+},
+{
+    title: "Project C",
+    overlayText: "View the contents of Project C.",
+    cardImage: "https://www.kasandbox.org/programming-images/landscapes/lake.png",
+    imageAltText: "A beautiful landscape from Khan Academy's sandbox.",
+    linkURL: "./projects/project-c.html",
+},
+{
+    title: "Project D",
+    overlayText: "View the contents of Project D.",
+    cardImage: "https://www.kasandbox.org/programming-images/landscapes/lake.png",
+    imageAltText: "A beautiful landscape from Khan Academy's sandbox.",
+    linkURL: "./projects/project-d.html",
+}];
+
+const container = document.getElementById("row");
+
+projectsList.forEach((item, itemID) => {
+    // Create card element:
+    const card = document.createElement("div");
+    card.classList = "card-body";
+
+    // Define card content:
+    const content = `
+        <div class="quad-column">
+            <div class="card-body" data-parent="#row">
+                <a href="${item.linkURL}">
+                    <div class="card-overlay">
+                        <p class="card-overlay-text">${item.overlayText}</p>
+                    </div>
+                </a>
+                <img class="centered" src="${item.cardImage}" alt="${item.imageAltText}">
+                <h2>${item.title}</h2>
+            </div>
+        </div>
+    `;
+    // Append each created element to the container:
+    container.innerHTML += content;
+})
